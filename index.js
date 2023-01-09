b8ae90aacf5a35488a04013c922cf8a6d75b174f
Fernando <fernandostarlin1012@gmail.com>
2023-01-09 14:33:11 -0400

    Working with objects in JS

--- index.js
+++ index.js
@@ -12,3 +12,27 @@ let age = 30; //Number Literal
 let isApproved = true; // Boolean Literal
 let firstName; // By default is undefined
 let selectedColor = null; // We use it in situations we want to clear the value of a variable
+
+
+// Reference Type (OBJECTS)
+
+let person = {
+    name: 'Fernan',
+    age: 22
+};
+
+// Dot notation to access the elements
+person.name = 'John';
+person.age = 25;
+
+// Bracket notation
+person['name'] = 'Mary';
+
+console.log(person);
+
+// Dynamic way
+
+let userSelection = 'name';
+
+person[userSelection] = 'Peter';
+console.log(person);
