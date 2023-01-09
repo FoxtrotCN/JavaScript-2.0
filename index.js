d320b0ec51a6887fa8d2ff9e837c2ae93a69057d
Fernando <fernandostarlin1012@gmail.com>
2023-01-09 13:18:57 -0400

    Primitive Value types

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-// Const Values
+// Primitive / Value Types
+/*
+* Strings
+* Numbers
+* Booleans
+* Undefined
+* Null
+ */
 
-const interestRate = 0.3;
-interestRate = 1;
-console.log(interestRate);
-
-// Const values cannot be change
+let name = 'Fernan'; //String Literal
+let age = 30; //Number Literal
+let isApproved = true; // Boolean Literal
+let firstName; // By default is undefined
+let selectedColor = null; // We use it in situations we want to clear the value of a variable
