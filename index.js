35784a8ca61be8bc0cc6aaa3802b02ddd197ba3c
Fernando <fernandostarlin1012@gmail.com>
2023-01-09 13:12:19 -0400

    Working with variables in JS

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hello World');
+// Declaring variables
+
+let name = 'Fernando';
+console.log(name);
+
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a number
+// Cannot contain a space or hyphen
+
+// Long variables labels should use CamelNotation (let firstName)
+
+
