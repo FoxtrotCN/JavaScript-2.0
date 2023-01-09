613e2fd8753aa5b40ea36cbcc30ddfb46f3d2b86
Fernando <fernandostarlin1012@gmail.com>
2023-01-09 16:17:12 -0400

    Here we've worked with the power of the logical operator OR (||) in JS when We are not working with booleans values.

--- index.js
+++ index.js
@@ -21,14 +21,26 @@ console.log(elegibleForloan);*/
 
 // NOT !: Change the operand value
 
-let higIncome = false;
+/*let higIncome = false;
 let goodCreditScore = false;
 let elegibleForloan = higIncome || goodCreditScore;
 console.log('Elegible: ' + elegibleForloan);
 
 let applicationRefused = !elegibleForloan;
-console.log('Refused: ' + applicationRefused);
+console.log('Refused: ' + applicationRefused);*/
 
+// Using the OR || operator in non-boolean values:
+// Real life example: Let's assume we're building an app
+// where the user is buying a t-shirt,
+// and he needs to pick a color, if not,
+// we set a color by default.
 
+let userColor = 'red';
+let defaultColor = 'blue';
+console.log(userColor || defaultColor); // We got red
 
+// Now, suppose the user color is empty or undefined:
 
+let userColor2 = '';
+let defaultColor2 = 'blue';
+console.log(userColor2 || defaultColor2);
