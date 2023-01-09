b20c96a20a61869e052c78be5fdccded38e2ba9d
Fernando <fernandostarlin1012@gmail.com>
2023-01-09 14:51:44 -0400

    Performing functions in js

--- index.js
+++ index.js
@@ -17,10 +17,18 @@ greet();
 greet('John'); // arguments
 greet('Mary');*/
 
+// Performing a Task
 function greet(name, lastName) { // Parameters
     console.log('Hello ' + name + ' ' + lastName);
 }
 
 greet('John', 'Smith'); // arguments
 greet('Mary');
+
+// Calculating a value
+
+function square(number) {
+    return number * number
+}
+console.log(square(2));
 
