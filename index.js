46d9c24c9e4d764ea93b3bd8aa9a779705f10fbc
Fernando <fernandostarlin1012@gmail.com>
2023-01-09 16:41:00 -0400

    This is a simple exercise to train our programming brain.

--- index.js
+++ index.js
@@ -1,46 +1,13 @@
-// Operators
+// Exercise
 
-// Logical Operator
-// AND / OR / NOT
+// Swapping variables values
 
-// AND &&: return true if both operands are TRUE
+let a = 'red';
+let b = 'blue';
 
-/*let higIncome = true;
-let goodCreditScore = true;
-let elegibleForloan = higIncome && goodCreditScore;
+let c = a;
+a = b;
+b = c;
 
-console.log(elegibleForloan);*/
-
-// OR ||: Returns True if one operand is TRUE
-
-/*let higIncome = true;
-let goodCreditScore = false;
-let elegibleForloan = higIncome || goodCreditScore;
-
-console.log(elegibleForloan);*/
-
-// NOT !: Change the operand value
-
-/*let higIncome = false;
-let goodCreditScore = false;
-let elegibleForloan = higIncome || goodCreditScore;
-console.log('Elegible: ' + elegibleForloan);
-
-let applicationRefused = !elegibleForloan;
-console.log('Refused: ' + applicationRefused);*/
-
-// Using the OR || operator in non-boolean values:
-// Real life example: Let's assume we're building an app
-// where the user is buying a t-shirt,
-// and he needs to pick a color, if not,
-// we set a color by default.
-
-let userColor = 'red';
-let defaultColor = 'blue';
-console.log(userColor || defaultColor); // We got red
-
-// Now, suppose the user color is empty or undefined:
-
-let userColor2 = '';
-let defaultColor2 = 'blue';
-console.log(userColor2 || defaultColor2);
+console.log(a);
+console.log(b);
