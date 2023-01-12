403ab197691de52e54b34cb6db974db4724311ea
Fernando <fernandostarlin1012@gmail.com>
2023-01-12 10:48:43 -0400

    Starting to work with conditional statements. In this section, the if...else

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-// Exercise
+// Conditional Statements
 
-// Swapping variables values
+// Hour
+// If hour is between 6am and 12pm: Good morning!
+// If it is between 12pm and 6pm: Good afternoon!
+// Otherwise: Good evening!
 
-let a = 'red';
-let b = 'blue';
+let hour = 11;
 
-let c = a;
-a = b;
-b = c;
+if (hour >= 6 && hour < 12)
+    console.log('Good morning!');
 
-console.log(a);
-console.log(b);
+
+else if (hour >= 12 && hour < 18)
+    console.log('Good afternoon!');
+
+
+else
+    console.log('Good evening!');
