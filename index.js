01c01543c95451e066ed1a7613c4591067533d87
Fernando <fernandostarlin1012@gmail.com>
2023-01-12 11:14:59 -0400

    do...while loop

--- index.js
+++ index.js
@@ -31,14 +31,20 @@ for (let i = 100; i >= 1; i--) {
 // statement
 // }
 
+//
+// let i = 0;
+//
+// while (i <= 100) {
+//     if (i % 2 !== 0) console.log(i);
+//     i++;
+//
+// }
+
+// do...while
 
 let i = 0;
 
-while (i <= 100) {
+do {
     if (i % 2 !== 0) console.log(i);
     i++;
-
-}
-
-
-
+} while (i <= 100)
