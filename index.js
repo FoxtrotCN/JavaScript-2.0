28cf93249b22b36455a196d29ad927a8764b7ac9
Fernando <fernandostarlin1012@gmail.com>
2023-01-12 11:05:29 -0400

    Starting to work with loops

--- index.js
+++ index.js
@@ -1,38 +1,22 @@
-// Conditional Statements
+// Loops
 
-// Hour
-// If hour is between 6am and 12pm: Good morning!
-// If it is between 12pm and 6pm: Good afternoon!
-// Otherwise: Good evening!
+// for
 
-let hour = 11;
+// for (initialExpression; condition; incrementExpression;) {
+//      statement
+// }
 
-if (hour >= 6 && hour < 12)
-    console.log('Good morning!');
-
-
-else if (hour >= 12 && hour < 18)
-    console.log('Good afternoon!');
-
-
-else
-    console.log('Good evening!');
-
-
-// switch and case
-
-let role;
-role = 'moderator';
-
-switch (role) {
-    case 'guest':
-        console.log('Guest User');
-        break;
-
-    case 'moderator':
-        console.log('Moderator User');
-        break;
-
-    default:
-        console.log('Unknown User');
+for (let i = 0; i < 5; i++) {
+    console.log('Hello Word', i);
 }
+
+// Assume we want to print the odd number between 1 and 100:
+// We can do this with the for loop and implementing the module operand
+// to retrieve the division reminder
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 !== 0)
+        console.log(i);
+}
+
+
