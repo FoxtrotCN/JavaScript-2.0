a8128b208cec444f12c0caf70f52794979ccb227
Fernando <fernandostarlin1012@gmail.com>
2023-01-12 12:06:48 -0400

    Exercises - Implement a function that takes 2 parameters that returns true if the images is landscape

--- index.js
+++ index.js
@@ -10,6 +10,24 @@ function maximumOfTwo(num1, num2) {
 let attempt1 = maximumOfTwo(2, 1);
 console.log(attempt1);
 
+// 2 - Implement a function that takes 2 parameters that returns true if the images is landscape
 
+// function isLandScape(width, height) {
+//     return (width > height) ? true : false;
+// }
+
+// let img1 = isLandScape(120, 100);
+// console.log(img1);
+
+
+// the code above is okay but is amateurish
+// a better approach could be:
+
+function isLandScape(width, height) {
+    return (width > height)
+}
+
+let img2 = isLandScape(120, 100);
+console.log(img2);
 
 
