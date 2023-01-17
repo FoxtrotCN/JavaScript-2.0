0940abb8b5ed0f7e7ca820127e41c23677f4624a
Fernando <fernandostarlin1012@gmail.com>
2023-01-17 08:30:07 -0400

    This is another way to create multiple objects from the same We don't want to repeat multiples lines of code If we got the same code, DRY principle.

--- index.js
+++ index.js
@@ -12,4 +12,23 @@ const circle = {
     }
 };
 
-circle.draw();
+// Factory Function
+
+// This is another way to create multiple objects from the same
+// We don't want to repeat multiples lines of code
+// If we got the same code, DRY principle.
+
+// We can create a function that creates an object
+
+function createCircle (radius) {
+    return {
+        radius,
+        draw() {
+            console.log('Draw');
+        }
+    };
+}
+
+const circle1 = createCircle(1);
+console.log(circle1);
+
