3dcd69eb4e001f688e103a6a960517e479f97157
Fernando <fernandostarlin1012@gmail.com>
2023-01-17 11:20:20 -0400

    This time we must implement 2 functions that evaluates the quality between 2 objects The first one if for the equality of properties or Key the second one is for the equality between the objects (which is always false)

--- index.js
+++ index.js
@@ -175,3 +175,41 @@ for (let key in address2) {
     console.log(key, address2[key]);
 }
 
+// 3 - Object Equality
+
+// This time we must implement 2 functions that evaluates the quality between 2 objects
+// The first one if for the equality of properties or Key
+// The second one is for the equality between the objects (which is always false)
+
+const address3 = new Address('2nd street', 'London', 11507);
+// address3.country = 'Dominican';
+// console.log(address2);
+
+const address4 = new Address('2nd street', 'London', 11507);
+// console.log(address2);
+
+function areEqual(address3, address4) {
+    let keyAddress3 = {};
+    let keyAddress4 = {};
+
+    for (let key in address3) {
+        keyAddress3 = key;
+    }
+
+    for (let key in address4) {
+        keyAddress4 = key;
+    }
+
+    console.log((keyAddress3 === keyAddress4));
+
+
+}
+
+areEqual(address3, address4);
+
+function areSame(address3, address4) {
+    console.log((address3 === address4));
+}
+
+areSame(address3, address4);
+
