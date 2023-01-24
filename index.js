1645c8735e6457cd97f839d57de95f15a58e793f
Fernando <fernandostarlin1012@gmail.com>
2023-01-24 19:24:32 -0400

    Exercises with Arrays
    Making an array from a given range

--- index.js
+++ index.js
@@ -1,35 +1,16 @@
-// Arrays
+// Arrays Exercises
+
+// Making an array from a given range
+
+const numbers = arrayFromRange(-20, -10);
+console.log(numbers);
+
+function arrayFromRange(min, max) {
+    const array = [];
+    for (let i = min; i <= max; i++) {
+        array.push(i);
+    }
+    return array;
+}
 
 
-// Testing the elements in an array
-
-const numbers = [1, 2, 3, 1, 2, 5, 6, 4, -10];
-
-// every() or some()
-
-const allPositive = numbers.every(value => {
-    return value >= 0;
-});
-
-const atLeastOnePositive = numbers.some(value => {
-    return value >= 0;
-});
-
-console.log(allPositive);
-console.log(atLeastOnePositive);
-
-// Filtering the elements in an array filter()
-
-const filtered = numbers.filter(value => {
-    return value >= 0;
-});
-
-console.log(filtered);
-
-// Mapping an array
-
-const items = numbers.map(n => {
-    return {value: n};
-});
-
-console.log(items);
