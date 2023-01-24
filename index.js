325986ee6924779d0f71ccf92f84f5b2a506f074
Fernando <fernandostarlin1012@gmail.com>
2023-01-24 17:46:09 -0400

    Finding elements in an array

--- index.js
+++ index.js
@@ -18,3 +18,16 @@ console.log(numbers);
 //Middle
 numbers.splice(2, 0, 'a', 'b');
 console.log(numbers);
+
+
+// Finding elements in an array
+
+// We got 2 methods, indexOf() and lastIndexOf()
+
+console.log(numbers.indexOf(2)); // Showing the first occurrence of an element in an array
+                                // if no elements, it returns -1
+
+console.log(numbers.lastIndexOf(3)); // Showing the last occurrence of an element in an array
+                                    // if no elements, it returns -1
+console.log(numbers.includes(0)); // Returns true or false if the given index exist.
+
