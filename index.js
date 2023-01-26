354382e102341582b56a0be438a127804351c258
Fernando <fernandostarlin1012@gmail.com>
2023-01-26 16:44:08 -0400

    In this exercise we should implement a function that move the element from its position to another

--- index.js
+++ index.js
@@ -2,16 +2,16 @@
 
 // Making an array from a given range
 
-const numbers = arrayFromRange(-20, -10);
-console.log(numbers);
-
-function arrayFromRange(min, max) {
-    const array = [];
-    for (let i = min; i <= max; i++) {
-        array.push(i);
-    }
-    return array;
-}
+// const numbers = arrayFromRange(-20, -10);
+// console.log(numbers);
+//
+// function arrayFromRange(min, max) {
+//     const array = [];
+//     for (let i = min; i <= max; i++) {
+//         array.push(i);
+//     }
+//     return array;
+// }
 
 // Implementing the includes method without implementing it
 // let numbers2 = [1, 2, 3]
@@ -29,15 +29,35 @@ function arrayFromRange(min, max) {
 //
 // }
 
-const numbers3 = [1, 2, 3, 4, 5];
-const excluded = except(numbers3, [1]);
-console.log(excluded);
+// const numbers3 = [1, 2, 3, 4, 5];
+// const excluded = except(numbers3, [1]);
+// console.log(excluded);
+//
+//
+// function except(array, excluded) {
+//     const output = [];
+//     for (let element of array)
+//         if (!excluded.includes(element))
+//             output.push(element);
+//     return output;
+// }
 
 
-function except(array, excluded) {
-    const output = [];
-    for (let element of array)
-        if (!excluded.includes(element))
-            output.push(element);
+// Moving an element
+
+const numbers = [1, 2, 3, 4];
+const output = move(numbers, 0, 1);
+console.log(output);
+
+function move(array, index, offset) {
+    const position = index + offset;
+    if (position >= array.length || position < 0) {
+        console.error('Invalid Offset');
+        return;
+    }
+
+    const output = [...numbers];
+    const element = output.splice(index, 1)[0];
+    output.splice(position, 0, element);
     return output;
 }
